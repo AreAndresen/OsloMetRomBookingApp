8f0578f36af6d23f7cc911806d3a8220d477e45c
AreAndresen <are_sandresen2@hotmail.com>
2019-11-15 08:09:53 +0100

    design

--- app/src/main/java/com/skole/s304114mappe3/MainActivityNy.java
+++ app/src/main/java/com/skole/s304114mappe3/MainActivityNy.java
@@ -337,7 +337,7 @@ public class MainActivityNy extends AppCompatActivity implements OnMapReadyCallb
         protected void onPostExecute(ArrayList<Rom> jsonArray) {
             markorerNy = jsonArray;
 
-            kjorJsonAlleReservasjoner();
+            kjorJsonAlleReservasjoner(); //RIKTIG ASYNC HER MTP AT HENTING AV ROM KJØRSS FØRST OG SÅ RESERVASJONER HER?-----------------------------------------------------------------------------------<<<
 
             for(int i = 0; i<markorerNy.size(); i++) {                                     //LEGGER INN ROMNR SOM DET SOM KOMMER VED TRYKK PÅ MARKØR
 
@@ -419,6 +419,8 @@ public class MainActivityNy extends AppCompatActivity implements OnMapReadyCallb
         @Override
         protected void onPostExecute(ArrayList<Reservasjon> jsonArray) {
             reservasjoner = jsonArray;
+
+            //RIKTIG ASYNC HER MTP?-----------------------------------------------------------------------------------<<<
             zoomTilSted();
         }
     }
@@ -453,8 +455,6 @@ public class MainActivityNy extends AppCompatActivity implements OnMapReadyCallb
         //mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(markorerNy.get(i).getLatLen()));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-
-
     }
 
 
@@ -483,11 +483,13 @@ public class MainActivityNy extends AppCompatActivity implements OnMapReadyCallb
         }
     }
 
+
     @Override
     public void onConnectionSuspended(int i) {
         Log.i(TAG, "Location services suspended. Please reconnect.");
 
     }
+
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
@@ -585,31 +587,6 @@ public class MainActivityNy extends AppCompatActivity implements OnMapReadyCallb
 
             for(Reservasjon r : reservasjoner) {
 
-                /*tilToast += romNr+": "+datoIdag+" MOT "+r.getDato()+": "+r.getTidFra()+" til  "+r.getTidTil()+"\n";
-                //HENTER DATO FRA BESTILLINGEN
-                String dato10 = r.getDato();*/
-
-
-                /*--------FORMATERER DATOENE FOR SAMMENLIGNING--------
-                SimpleDateFormat sdf0 = new SimpleDateFormat("dd.MM.yyyy");
-                Date dato20 = null;
-                Date dato40 = null;
-
-                try {
-                    dato20 = sdf0.parse(dato10);
-                    dato40 = sdf0.parse(datoIdag);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-
-                //--------SAMMENLIGNINGER AV FORMATERTE DATOER--------
-                //HVIS DATO ER I DAG
-                if((dato20.compareTo(dato40) == 0)) {
-                    tilToast+= "DATOEN ER LIKE";
-                }*/
-
-
                 if(r.getRomNr().equals(romNr)) {
 
 
@@ -645,29 +622,13 @@ public class MainActivityNy extends AppCompatActivity implements OnMapReadyCallb
                 reservasjonerTekst = "Ingen aktive reservasjoner i dag.";
             }
 
-            visBestillingsinfo(reservasjonerTekst);
+            visReservasjonssinfo(reservasjonerTekst);
 
-            /*final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Reservasjoner i dag")
-                    .setMessage(reservasjonerTekst)
-                    .setCancelable(true)
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                            dialog.dismiss();
-                        }
-                    });
-                    /*.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                        public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                            dialog.cancel();
-                        }
-                    });
-            final AlertDialog alert = builder.create();
-            alert.show();*/
         }
     }
 
     //--------OPPRETTER SEBESTILLINGSINFODIALOG--------
-    private void visBestillingsinfo(String reservasjoner)  {
+    private void visReservasjonssinfo(String reservasjoner)  {
 
         //OPPRETTER NYTT DIALOGFRAGMENT
         SeReservasjonerDialog rFragment = new SeReservasjonerDialog();
@@ -727,142 +688,3 @@ public class MainActivityNy extends AppCompatActivity implements OnMapReadyCallb
         finish();
     }
 }
-
-
-
-/*package com.skole.s304114mappe3;
-
-
-import androidx.appcompat.app.AppCompatActivity;
-
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
-import android.widget.Toolbar;
-
-
-public class MainActivity extends AppCompatActivity {
-
-    //--------KNAPPER-------
-    Button seRomKnapp, registrerRomKnapp, reserverRomKnapp, innstillingerKnapp;
-
-    ImageView logo;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-        toolbar.inflateMenu(R.menu.minmeny);
-        //toolbar.setNavigationIcon(R.drawable.ic_action_name); //android: //src="@drawable/logo"
-        //toolbar.setTitleTextColor(getResources().getColor(R.color.colorText2));
-        setActionBar(toolbar);
-
-
-        logo = findViewById(R.id.logo2);
-
-        //--------KNAPPER--------
-        seRomKnapp = findViewById(R.id.seRomKnapp);
-        registrerRomKnapp = findViewById(R.id.registrerRomKnapp);
-        reserverRomKnapp = findViewById(R.id.reserverRomKnapp);
-        innstillingerKnapp = findViewById(R.id.innstillingerKnapp);
-
-
-        //--------LISTENERS--------
-        //KLIKK RESTURANTER
-        //--------LISTENERS--------
-        //KLIKK RESTURANTER
-        logo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent_startspill = new Intent (MainActivity.this, MainActivity.class);
-                startActivity(intent_startspill);
-                finish();
-            }
-        });
-
-
-        seRomKnapp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent_startspill = new Intent (MainActivity.this, Kart.class);
-                startActivity(intent_startspill);
-                finish();
-            }
-        });
-
-        //KLIKK VENNER
-        registrerRomKnapp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent_statistikk = new Intent (MainActivity.this, RegistrerRom.class);
-                startActivity(intent_statistikk);
-            }
-        });
-
-        //KLIKK BESTILLINGER
-        reserverRomKnapp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent_preferanser = new Intent (MainActivity.this, ReserverRom.class);
-                startActivity(intent_preferanser);
-                finish();
-            }
-        });
-
-        //KLIKK INNSTILLINGER
-        innstillingerKnapp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Intent intent_preferanser = new Intent (MainActivity.this, NotifikasjonFragment.class);
-                //startActivity(intent_preferanser);
-            }
-        });
-        //--------SLUTT LISTENERS--------
-
-    }//-------CREATE SLUTTER---------
-
-
-    //En metode for å lage To o l b a rfra minmeny.xml
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.minmeny, menu);
-        return true;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.seRom:
-                Intent intent_startspill = new Intent (MainActivity.this, Kart.class);
-                startActivity(intent_startspill);
-                finish();
-                break;
-            case R.id.registrerRom:
-                Intent intent_statistikk = new Intent (MainActivity.this, RegistrerRom.class);
-                startActivity(intent_statistikk);
-                break;
-            case R.id.reserverRom:
-                Intent intent_preferanser = new Intent (MainActivity.this, ReserverRom.class);
-                startActivity(intent_preferanser);
-                finish();
-                break;
-            default:
-                // If wegothere, theuser'saction wasnot recognized
-                // Invokethesuperclassto handle it.
-                return super.onOptionsItemSelected(item);
-        }
-        return true;
-    }
-}*/
